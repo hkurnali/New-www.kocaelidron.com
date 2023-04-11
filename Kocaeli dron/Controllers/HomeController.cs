cce427fcfdb9ed6d8646682f36ab89aafc3786c4
hkurnali <huseyinkurnali@hotmail.com>
2023-04-11 14:19:06 +0300

    Add project files.

---
+++ Kocaeli dron/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kocaeli_dron.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Anasayfa
+            ()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimda()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult İletisim()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Hizmetlerim()
+        {
+            return View();
+        }
+       
+    }
+}
